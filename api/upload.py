3225c97b73bd5c845e2565b360c71c73248fa587
Matt Levine <mtlevine0@gmail.com>
2016-08-01 23:59:10 +0000

    Broke out save functionality.

--- api/upload.py
+++ api/upload.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, request, redirect, url_for
+from flask import Flask, Blueprint, request, redirect, url_for, Response
 from werkzeug import secure_filename
 import os
 
@@ -8,13 +8,29 @@ ALLOWED_EXTENSIONS = set(['txt','gif','png','jpg'])
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-@upload_api.route('/upload', methods=['POST','PUT'])
-def upload():
-    file = request.files['file']
+    
+def save_file(file):
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join("/home/ubuntu/workspace/static", filename))
-        print(filename)
+        try:
+            file.save(os.path.join("/home/ubuntu/workspace/static/upload", filename))
+        except IOError:
+            # TODO: Handle IOError exception
+            filename = False
+    else:
+        # TODO: handle multiple returns
+        filename = False
+    
+    return filename
 
-    return redirect("/static/" + filename, code=301)
+@upload_api.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    filename = save_file(file)
+    
+    if filename:
+        response = redirect("/static/upload/" + filename, code=302)
+    else:
+        response = Response("Internal Error" , status=500)
+
+    return response
