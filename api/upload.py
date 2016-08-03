5bafc35b1830eda73fdd20f6a43879b19531650f
Matt Levine <mtlevine0@gmail.com>
2016-08-03 00:18:04 +0000

    Database ORM integration with peewee. Broke out DB constants into properties.txt.

--- api/upload.py
+++ api/upload.py
@@ -1,6 +1,9 @@
 from flask import Flask, Blueprint, request, redirect, url_for, Response
 from werkzeug import secure_filename
 import os
+import peewee as pw
+import database as db
+import uuid
 
 upload_api = Blueprint('upload_api', __name__)
 
@@ -9,11 +12,12 @@ ALLOWED_EXTENSIONS = set(['txt','gif','png','jpg'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
     
-def save_file(file):
+def save_file(file, imageID):
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
+        fileExtension = filename.split(".")[1]
         try:
-            file.save(os.path.join("/home/ubuntu/workspace/static/upload", filename))
+            file.save(os.path.join("/home/ubuntu/workspace/static/upload", imageID + "." + fileExtension))
         except IOError:
             # TODO: Handle IOError exception
             filename = False
@@ -25,11 +29,15 @@ def save_file(file):
 
 @upload_api.route('/upload', methods=['POST'])
 def upload():
-    file = request.files['file']
-    filename = save_file(file)
     
+    file = request.files['file']
+    imageID = str(uuid.uuid4())[:7]
+    filename = save_file(file, imageID)
+    fileExtension = filename.split(".")[1]
+
     if filename:
-        response = redirect("/static/upload/" + filename, code=302)
+        db.Image.create(fileName=filename,imageID=imageID)
+        response = redirect("/static/upload/" + imageID + "." + fileExtension, code=302)
     else:
         response = Response("Internal Error" , status=500)
 
--- flaskphoto.py
+++ flaskphoto.py
@@ -2,11 +2,7 @@ from flask import Flask
 from api import upload
 import os
 
-UPLOAD_FOLDER = '/tmp/'
-ALLOWED_EXTENSIONS = set(['txt'])
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def main():
@@ -15,6 +11,6 @@ def main():
 PORT = int(os.getenv('PORT', '8080'))
 
 if __name__ == '__main__':
-
+    
     app.register_blueprint(upload.upload_api)
     app.run(debug=True, host="0.0.0.0", port=PORT)
